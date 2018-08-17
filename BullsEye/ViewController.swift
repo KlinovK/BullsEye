ebe7ce344478a941be816d2183fc2ef7cd5e09be
kost9klinov <kost9klinov>
2018-08-17 16:24:53 +0300

    added score and rounds

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,25 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var randomValue: UILabel!
+    @IBOutlet weak var scoreLblb: UILabel!
+    @IBOutlet weak var roundNumberLbl: UILabel!
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var roundNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        targetValue = Int.random(in: 1...100)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        scoreLblb.text = "\(0)"
+        roundNumberLbl.text = "\(0)"
+        
     }
 
 
     @IBAction func showAlert(){
-       let alert = UIAlertController(title: "Hello World!", message: "this is my first App", preferredStyle: .alert)
+        let  diff = abs(targetValue - currentValue)
+        var points = 100 - diff
+        
+      score += points
+        
+        
+        
+        let title: String
+        if diff == 0 {
+            title = "Perfect"
+            points += 100
+        } else if diff < 5 {
+            title = "You almost had it!"
+            points += 50
+        } else if diff < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "not even close..."
+        }
+        let message = "You scored: \(points)"
+       let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
+        startNewRound()
     }
+    
+
+    @IBAction func sliderMoved(_ slider : UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+         roundNumber += 1
+        slider.value = Float(currentValue)
+        updateLabeles()
+    }
+    
+    func updateLabeles() {
+        randomValue.text = "\(targetValue)"
+        scoreLblb.text = "\(score)"
+        roundNumberLbl.text = "\(roundNumber)"
+
+    }
+   
     
 }
 
